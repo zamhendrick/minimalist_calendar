445a2762914cd9f7978f187e709cde66a3ad5538
zamhendrick <zamhendrick@gmail.com>
2026-06-14 03:40:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const dateObj = new Date();
+const fontSizeLandscape = "35cqh";
+const fontSizePortrait = "45cqw";
+
 
 // MONTH
 const month = dateObj.toLocaleDateString("us-en", { month: "short" });
@@ -13,14 +16,19 @@ document.getElementsByClassName("grid__card--day")[1].getElementsByClassName("gr
 function getScreenOrientation() {
     if (window.innerWidth > window.innerHeight) {
         // console.log("landscape")
-        document.getElementsByClassName("grid__card__content")[0].style.fontSize = "30cqh";
-        document.getElementsByClassName("grid__card__content")[1].style.fontSize = "30cqh";
-        document.getElementsByClassName("grid__card__content")[2].style.fontSize = "30cqh";
+        document.getElementsByClassName("grid")[0].style.height = "100%";
+        document.getElementsByClassName("grid")[0].style.padding = "4cqh";
+        document.getElementsByClassName("grid__card__content")[0].style.fontSize = fontSizeLandscape;
+        document.getElementsByClassName("grid__card__content")[1].style.fontSize = fontSizeLandscape;
+        document.getElementsByClassName("grid__card__content")[2].style.fontSize = fontSizeLandscape;
     } else if (window.innerWidth < window.innerHeight) {
         // console.log("portrait")
-        document.getElementsByClassName("grid__card__content")[0].style.fontSize = "30cqw";
-        document.getElementsByClassName("grid__card__content")[1].style.fontSize = "30cqw";
-        document.getElementsByClassName("grid__card__content")[2].style.fontSize = "30cqw";
+        document.getElementsByClassName("grid")[0].style.height = "fit-content";
+        document.getElementsByClassName("grid")[0].style.width = "100%";
+        document.getElementsByClassName("grid")[0].style.padding = "4cqw";
+        document.getElementsByClassName("grid__card__content")[0].style.fontSize = fontSizePortrait;
+        document.getElementsByClassName("grid__card__content")[1].style.fontSize = fontSizePortrait;
+        document.getElementsByClassName("grid__card__content")[2].style.fontSize = fontSizePortrait;
     }
 
     // console.log(window.innerWidth, window.innerHeight)

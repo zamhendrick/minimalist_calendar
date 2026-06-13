93ecb56e4a603dd4996312d85891fd263c6c231b
zamhendrick <zamhendrick@gmail.com>
2026-06-14 03:11:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ const day = dateObj.toLocaleDateString("us-en", { day:"2-digit"})
 document.getElementsByClassName("grid__card--day")[0].getElementsByClassName("grid__card__content")[0].append(day[0]);
 document.getElementsByClassName("grid__card--day")[1].getElementsByClassName("grid__card__content")[0].append(day[1]);
 // console.log(monthString);
+
+function getScreenOrientation() {
+    if (window.innerWidth > window.innerHeight) {
+        // console.log("landscape")
+        document.getElementsByClassName("grid__card__content")[0].style.fontSize = "30cqh";
+        document.getElementsByClassName("grid__card__content")[1].style.fontSize = "30cqh";
+        document.getElementsByClassName("grid__card__content")[2].style.fontSize = "30cqh";
+    } else if (window.innerWidth < window.innerHeight) {
+        // console.log("portrait")
+        document.getElementsByClassName("grid__card__content")[0].style.fontSize = "30cqw";
+        document.getElementsByClassName("grid__card__content")[1].style.fontSize = "30cqw";
+        document.getElementsByClassName("grid__card__content")[2].style.fontSize = "30cqw";
+    }
+
+    // console.log(window.innerWidth, window.innerHeight)
+}
+
+window.onload = getScreenOrientation;
+window.onresize = getScreenOrientation;

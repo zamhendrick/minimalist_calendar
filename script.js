03117e6c4b99446853790f2d5fe107dc7faeae6a
zamhendrick <zamhendrick@gmail.com>
2026-06-14 08:55:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log(navigator.userAgent)
 
 const dateObj = new Date();
-const fontSizeLandscape = "30vh";
-const fontSizePortrait = "30vw";
+const fontSizeLandscape = "30cqh";
+const fontSizePortrait = "30cqw";
 
 // MONTH
 const month = dateObj.toLocaleDateString("us-en", { month: "short" });
@@ -16,27 +16,21 @@ document.getElementsByClassName("grid__card--day")[1].getElementsByClassName("gr
 
 function getScreenOrientation() {
     if (window.innerWidth > window.innerHeight) {
-        // console.log("landscape")
         document.getElementsByClassName("grid")[0].style.height = "100%";
-        document.getElementsByClassName("grid")[0].style.width = "fit-content";
-        document.getElementsByClassName("grid")[0].style.padding = "4cqh";
-        document.getElementsByClassName("grid")[0].style.gap = "4cqh";
+        document.getElementsByClassName("grid")[0].style.width = "unset";
         document.getElementsByClassName("grid__card__content")[0].style.fontSize = fontSizeLandscape;
         document.getElementsByClassName("grid__card__content")[1].style.fontSize = fontSizeLandscape;
         document.getElementsByClassName("grid__card__content")[2].style.fontSize = fontSizeLandscape;
     } else if (window.innerWidth < window.innerHeight) {
-        // console.log("portrait")
-        document.getElementsByClassName("grid")[0].style.height = "fit-content";
+        document.getElementsByClassName("grid")[0].style.height = "unset";
         document.getElementsByClassName("grid")[0].style.width = "100%";
-        document.getElementsByClassName("grid")[0].style.padding = "4cqw";
-        document.getElementsByClassName("grid")[0].style.gap = "4cqw";
         document.getElementsByClassName("grid__card__content")[0].style.fontSize = fontSizePortrait;
         document.getElementsByClassName("grid__card__content")[1].style.fontSize = fontSizePortrait;
         document.getElementsByClassName("grid__card__content")[2].style.fontSize = fontSizePortrait;
     }
 
-    // console.log(window.innerWidth, window.innerHeight)
+    console.log(window.innerWidth, window.innerHeight)
 }
 
-window.onload = getScreenOrientation;
 window.onresize = getScreenOrientation;
+window.onload = getScreenOrientation;
